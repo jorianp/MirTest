d2015a848cec5b9fff6773c1ff120f37d0736a3a
Alex Eliseev <eajorianp@gmail.com>
2019-11-14 18:24:13 +0300

    Add adaptive and animation

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -3,6 +3,22 @@ const android = document.querySelector('.intro__button-android');
 const iphone = document.querySelector('.intro__ios');
 const samsung = document.querySelector('.intro__android');
 
+iphone.addEventListener('click', () => {
+    ios.classList.add('button-checked');
+    android.classList.remove('button-checked');
+
+    iphone.classList.add('intro__ios-chk');
+    samsung.classList.remove('intro__android-dis');
+});
+
+samsung.addEventListener('click', () => {
+    android.classList.add('button-checked');
+    ios.classList.remove('button-checked');
+
+    iphone.classList.remove('intro__ios-chk');
+    samsung.classList.add('intro__android-dis');
+});
+
 ios.addEventListener('click', () => {
     ios.classList.add('button-checked');
     android.classList.remove('button-checked');
